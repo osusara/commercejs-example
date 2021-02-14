01e5386122ad55468daf52828fd28fbf31110a71
Osusara Kammalawatta <osusarak96@gmail.com>
2021-02-14 22:49:38 +0530

    Added cart functionality

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -11,25 +11,35 @@ import { AddShoppingCart } from "@material-ui/icons";
 
 import useStyles from "./styles";
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
-      
+      <CardMedia
+        className={classes.media}
+        image={product.media.source}
+        title={product.name}
+      />
+
       <CardContent>
         <div className={classes.cardContent}>
           <Typography variant="h5" gutterBottom>
             {product.name}
           </Typography>
-          <Typography variant="h5">{product.price.formated_with_symbol}</Typography>
+          <Typography variant="h5">
+            {product.price.formated_with_symbol}
+          </Typography>
         </div>
-        <Typography variant="body2" color="textSecondary" dangerouslySetInnerHTML={{ __html: product.description }} />
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          dangerouslySetInnerHTML={{ __html: product.description }}
+        />
       </CardContent>
 
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton arira-label="Add to Cart">
+        <IconButton arira-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
           <AddShoppingCart />
         </IconButton>
       </CardActions>
